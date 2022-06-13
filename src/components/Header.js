a124f2851fec5de7c7fb4fb9bb2ecf6ca5ab48e9
Ha Anna <its.haanna@gmail.com>
2022-06-13 13:46:14 +0900

    CHANGE: trying out different settings

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,15 @@
 import React from "react";
-import { BrowserView, MobileView } from 'react-device-detect';
 
 export default function Header(props) {
   const downloadCV = props.cv
-  const { langButton, toggleLanguage, cvFIle } = props
-
-  function saveAsPdf(pdf) {
-    const iframe = document.createElement('iframe');
-    iframe.style.visibility = "none";
-    iframe.src = pdf;
-    document.body.appendChild(iframe);
-    window.focus()
-    window.print()
-  }
+  const { langButton, toggleLanguage, cvFile } = props
 
   return (
     <header >
       <h1 className="visually-hidden">Ha Anna's CV</h1>
-      <BrowserView>
-        <button onClick={saveAsPdf} className="download-button">{downloadCV}</button>
-      </BrowserView>
-      <MobileView>
-        <form action={`/${cvFIle}`}>
-          <button className="download-button">{downloadCV}</button>
-        </form>
-      </MobileView>
+      <form action={`/${cvFile}`}>
+        <button className="download-button">{downloadCV}</button>
+      </form>
       <button onClick={toggleLanguage} className="lang-button" aria-label="Change language">{langButton}</button>
     </header>
   )
